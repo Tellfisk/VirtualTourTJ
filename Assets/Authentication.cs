ec18bef39f658f770371e78ed3d2d12eb7893973
Zylvian <33404765+Zylvian@users.noreply.github.com>
2020-11-30 01:25:40 +0100

    progress on downloading

--- Assets/Authentication.cs
+++ Assets/Authentication.cs
@@ -10,13 +10,13 @@ using System;
 using System.IO;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 public class Authentication : MonoBehaviour
 {
     private FirebaseAuth auth;
     private FirebaseFirestore db;
     private FirebaseStorage storage;
-    private StorageReference reference;
 
     public GameObject usernameField;
     public GameObject passwordField;
@@ -79,21 +79,7 @@ public class Authentication : MonoBehaviour
         }
     }
 
-    private void DownloadRoutine(string firebaseFolderName)
-    {
-        string filename = "tour.json";
-        // Create local filesystem URL
-        string localDLPath = Path.Combine(Application.streamingAssetsPath);
-        string local_url = localDLPath + "/" + firebaseFolderName; // + "/" + filename;
-        reference = storage.GetReference("Tours/" + firebaseFolderName);
-        //reference = reference.Child(filename);
-        Debug.Log("Download to: " + local_url);
-
-        // Download tour.json to the local filesystem
-        System.Threading.Tasks.Task downloadTask = reference.GetFileAsync(local_url);
-        //yield return new WaitUntil(() => downloadTask.IsCompleted);
-        //TODO: Display whether downloads were successful
-    }
+   
 
     IEnumerator LoadVirtualTour(string jsonPath)
     {
@@ -130,9 +116,34 @@ public class Authentication : MonoBehaviour
         yield return imageNames;
     }
 
+    private Task DownloadJson(string firebaseFolderName, string localDLPath)
+    {
+        string filename = "tour.json";
+
+        StorageReference dirReference = storage.GetReference("Tours/" + firebaseFolderName);
+        StorageReference jsonReference = dirReference.Child(filename);
+
+        string localJsonPath = Path.Combine(localDLPath, filename);
+
+        Debug.Log("Download to: " + localJsonPath);
+
+        // Download tour.json to the local filesystem
+        //System.Threading.Tasks.Task downloadTask = jsonReference.GetFileAsync(localDLPath);
+        //yield return new WaitUntil(() => downloadTask.IsCompleted);
+
+        Task taskman = jsonReference.GetFileAsync(localJsonPath);
+
+        taskman.ContinueWith(task => { Debug.Log("done with download!! -- " + Task.CurrentId); Debug.Log(task.IsFaulted); });
+        return taskman;
+
+    }
+
     private void FindAndDownloadFirebaseFolders()
     {
+        // TODO: only get tours for specific user.
         CollectionReference usersRef = db.Collection("users");
+        
+
         usersRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
         {
             List<string> tourNames = new List<string>();
@@ -148,29 +159,36 @@ public class Authentication : MonoBehaviour
                     tourNames.Add(lol);
                 }
             }
-            Debug.Log(tourNames);
             return tourNames;
+
         }).ContinueWith(task => {
             List<string> tourNames = task.Result;  //TODO: Contains duplicates
-            string localDLPath = Path.Combine(Application.streamingAssetsPath);
-            var xx = storage.GetReference("Tours");
-            var xxx = storage.GetReference("Tours/");
+            
+
+            var tasks = new List<Task>();
+            
+
+            Directory.Delete(Path.Combine(Application.streamingAssetsPath, "Tours"), true);
+
             foreach (string tourName in tourNames)
             {
-                string tourFolder = localDLPath + "/" + tourName;
-                Debug.Log("YYYOOOOoo " + tourName);
-
-                DownloadRoutine(tourName);
-                LoadVirtualTour(tourFolder);
-                continue;  //TODO: Bad workaround
+                string localDLPath = Path.Combine(Application.streamingAssetsPath, "Tours", tourName);
 
                 try
                 {
-                    if (!Directory.Exists(tourFolder))
+                    if (!Directory.Exists(localDLPath))
                     {
-                        Directory.CreateDirectory(tourFolder);
-                        Debug.Log("YYYOOOOoo " + tourName);
-                        DownloadRoutine(tourName);
+                        //Debug.Log("Path does not exist for: " + tourName);
+                        Directory.CreateDirectory(localDLPath);
+                        Task taskman = DownloadJson(tourName, localDLPath);
+                        tasks.Add(taskman);
+                        
+                        Debug.Log("penis " + taskman.Status);
+
+                    }
+                    else
+                    {
+                        //Debug.Log(String.Format("Directory for {0} already exists", tourName));
                     }
                 }
                 catch (IOException ex)
@@ -178,6 +196,16 @@ public class Authentication : MonoBehaviour
                     Console.WriteLine(ex.Message);
                 }
             }
+            Task.WaitAny(tasks.ToArray());
+
+            Debug.Log("en ferdig");
+            Debug.Log(tasks.Count);
+            
+            Task.WaitAll(tasks.ToArray());
+
+        }).ContinueWith(task => { 
+            Debug.Log("spaghetti"); 
         });
+        Debug.Log("UTENFOOOR");
     }
 }
